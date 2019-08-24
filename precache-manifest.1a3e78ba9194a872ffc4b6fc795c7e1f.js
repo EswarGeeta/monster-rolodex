57693dbae616aaff2e8b1aa1fbf7187af2ce26c4
Eswar Andavarapu <eswararao.andavarapu@optiv.com>
2019-08-24 06:20:54 +0530

    Updates

---
+++ precache-manifest.1a3e78ba9194a872ffc4b6fc795c7e1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1744b68459810fb322c5a1b4a5504788",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "321e0d10370054822c46",
+    "url": "/monster-rolodex/static/css/main.31bbf356.chunk.css"
+  },
+  {
+    "revision": "607e395712115653f754",
+    "url": "/monster-rolodex/static/js/2.45dda9c7.chunk.js"
+  },
+  {
+    "revision": "321e0d10370054822c46",
+    "url": "/monster-rolodex/static/js/main.ec7d9095.chunk.js"
+  },
+  {
+    "revision": "2a0841746d3b50a5d128",
+    "url": "/monster-rolodex/static/js/runtime~main.cc3a76ac.js"
+  }
+]);
